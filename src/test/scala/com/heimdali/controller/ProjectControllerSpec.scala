010f4b8f2ae64a0b85f0517d8fc105c5b59dce45
Benny Thompson <benny@jotunn.io>
2017-08-08 12:01:31 -0500

    Added test to make sure read-only values don't get accepted

--- src/test/scala/com/heimdali/controller/ProjectControllerSpec.scala
+++ src/test/scala/com/heimdali/controller/ProjectControllerSpec.scala
@@ -25,7 +25,7 @@ class ProjectControllerSpec
 
   behavior of "ProjectController"
 
-  it should "do something" in {
+  it should "create a project" in {
     val json = Json.obj(
       "name" -> "sesame",
       "purpose" -> "to do something cool"
@@ -55,6 +55,34 @@ class ProjectControllerSpec
     val creator = (jsonResponse \ "created_by").asOpt[String]
     creator shouldBe defined
     creator.get shouldBe "username"
+  }
+
+  it should "not accept read-only fields" in {
+    val oldDate = new DateTime(2010, 1, 1, 0, 0, 0)
+    val wrongUser = "johnsmith"
+    val fakeId = 999
+    val json = Json.obj(
+      "id" -> fakeId,
+      "name" -> "sesame",
+      "purpose" -> "to do something cool",
+      "created" -> oldDate,
+      "created_by" -> wrongUser
+    )
+
+    val request = FakeRequest(POST, "/projects")
+      .withHeaders(AUTHORIZATION -> "Bearer AbCdEf123456")
+      .withHeaders(CONTENT_TYPE -> "application/json")
+      .withJsonBody(json)
+
+    val rootCall = route(app, request).get
+
+    status(rootCall) should be(CREATED)
+
+    val jsonResponse = contentAsJson(rootCall).as[JsObject]
+
+    (jsonResponse \ "id").as[Int] should not be fakeId
+    (jsonResponse \ "created").as[DateTime] should not be oldDate
+    (jsonResponse \ "created_by").as[String] should not be wrongUser
   }
 
   import play.api.inject.bind
