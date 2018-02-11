52c446c1d7a6f5ce33f375f7092c3dbbc7c47f41
Benny Thompson <benny@jotunn.io>
2018-02-11 00:47:59 -0600

    Added workspace formatting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,17 +3,36 @@ import './Navigation.css';
 import logo from './logo.png';
 import Profile from "./Profile";
 import ClusterInfo from "./ClusterInfo";
+import {NavLink} from "react-router-dom";
 
 const Navigation = () => {
     return (
         <div className="Navigation">
             <div className="Navigation-left">
-                <img src={logo} alt="logo" className="Navigation-logo" />
-                Heimdali
+                <div className="Navigation-title">
+                    <img src={logo} alt="logo" className="Navigation-logo"/>
+                    Heimdali
+                </div>
+                <ul className="Navigation-nav">
+                    <li>
+                        <NavLink to="/" activeStyle={{
+                            color: "lightgray"
+                        }}>
+                            My Workspace
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/shared" activeStyle={{
+                            color: "lightgray"
+                        }}>
+                            Shared Workspaces
+                        </NavLink>
+                    </li>
+                </ul>
             </div>
             <div className="Navigation-right">
-                <ClusterInfo />
-                <Profile />
+                <ClusterInfo/>
+                <Profile/>
             </div>
         </div>
     );
--- src/components/UserWorkspaceDisplay.js
+++ src/components/UserWorkspaceDisplay.js
@@ -1,21 +1,33 @@
 import React from "react";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { tomorrowNightBlue } from 'react-syntax-highlighter/styles/hljs';
 import "./UserWorkspaceDisplay.css";
 
 const UserWorkspaceDisplay = ({workspace, cluster}) => {
+    const codeString = "SELECT * FROM " + workspace.database;
     return (
         <div className="UserWorkspaceDisplay">
             <h2 className="UserWorkspaceDisplay-notice">
-                Your workspace is set up!
+                Your workspace is set up<br />on "{cluster.name}"!
             </h2>
-            <h3 className="UserWorkspaceDisplay-sub">
-                here's the info for {cluster.name}
-            </h3>
             <dl className="UserWorkspaceDisplay-info">
                 <dt>Database</dt>
                 <dd>{workspace.database}</dd>
                 <dt>Data Directory</dt>
                 <dd>{workspace.data_directory}</dd>
             </dl>
+            <SyntaxHighlighter language="sql" customStyle={{
+                display: "flex",
+                width: "100%",
+                height: 50,
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: 15,
+                marginBottom: 15
+            }} style={tomorrowNightBlue}>
+                {codeString}
+            </SyntaxHighlighter>
+            Happy Coding!
         </div>
     );
 };
