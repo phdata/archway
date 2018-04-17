7f372e26f6e053849d4bef0b8c095237f5748a26
Benny Thompson <benny@jotunn.io>
2018-04-16 23:36:02 -0500

    Fixed parcel path reference

--- meta/heimdali_env.sh
+++ meta/heimdali_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export HEIMDALI_SECRET=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1`
-export HEIMDALI_API_HOME=$PARCELS_ROOT/$KAFKA_DIRNAME/usr/lib/heimdali-api
-export HEIMDALI_UI_HOME=$PARCELS_ROOT/$KAFKA_DIRNAME/usr/lib/heimdali-ui
+export HEIMDALI_SECRET=`date +%s | sha256sum | base64 | head -c 32`
+export HEIMDALI_API_HOME=$PARCELS_ROOT/$PARCEL_DIRNAME/usr/lib/heimdali-api
+export HEIMDALI_UI_HOME=$PARCELS_ROOT/$PARCEL_DIRNAME/usr/lib/heimdali-ui
