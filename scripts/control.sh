1a042a59b685a70a1c7aea89d7c8e82c2eeb3679
Benny Thompson <benny@jotunn.io>
2018-05-10 18:47:10 -0500

    Fixed typo and classpath issues

--- scripts/control.sh
+++ scripts/control.sh
@@ -22,7 +22,7 @@ case ${COMPONENT} in
             (start)
                 exec java -Djavax.security.auth.useSubjectCredsOnly=false \
                           -Djava.security.auth.login.config=${CONF_DIR}/jaas.conf \
-                          -cp ${CONF_DIR}:/usr/share/cmf/common_jars/mysql*.jar:/usr/share/cmf/common_jars/postgres*.jar:/opt/cloudera/parcels/CDH/jars/bcprov-jdk15-1.45.jar:/opt/cloudera/parcels/CDH/lib/hive/lib/hive-jdbc-standalone.jar:`hadoop classpath`:$HEIMDALI_API_HOME/heimdali-api.jar \
+                          -cp ${CONF_DIR}:/usr/share/java/mysql-connector-java.jar:/usr/share/cmf/common_jars/postgres*.jar:/opt/cloudera/parcels/CDH/jars/bcprov-jdk15-1.45.jar:/opt/cloudera/parcels/CDH/lib/hive/lib/hive-jdbc-standalone.jar:`hadoop classpath`:$HEIMDALI_API_HOME/heimdali-api.jar \
                           com.heimdali.Main
                 ;;
             (*)
@@ -34,7 +34,7 @@ case ${COMPONENT} in
         case ${CMD} in
             (start)
                 cd $HEIMDALI_UI_HOME
-                sed -i -e "s/%%BASE_URL%%/$HOST:$HEIMDALI_API_PORT/g" index.htm
+                sed -i -e "s/%%BASE_URL%%/$HOST:$HEIMDALI_API_PORT/g" index.html
                 exec python -m SimpleHTTPServer ${HEIMDALI_UI_PORT}
                 ;;
             (*)
