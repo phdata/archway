54a9b21c3c8951cc72399fecfde244cba7954fed
Benny Thompson <benny@jotunn.io>
2018-07-02 06:03:40 +0000

    Resolve "Fix deployed profile loading"

--- src/Auth/sagas.js
+++ src/Auth/sagas.js
@@ -1,9 +1,8 @@
 import { reset, stopSubmit } from 'redux-form';
-import { all, call, cancel, fork, put, take, select, takeLatest } from 'redux-saga/effects';
+import { all, call, cancel, fork, put, take, select } from 'redux-saga/effects';
+import { delay } from 'redux-saga';
 import * as Api from '../API';
 import * as actions from './actions';
-import { LOGIN_FAILURE, LOGOUT_REQUEST } from './actions';
-import { push } from 'react-router-redux';
 
 function* authorize(username, password) {
   try {
@@ -11,6 +10,7 @@ function* authorize(username, password) {
     const { access_token, refresh_token } = response;
     localStorage.setItem('requestToken', access_token);
     localStorage.setItem('refreshToken', refresh_token);
+    yield fork(tokenReady, { token: access_token });
     yield put(actions.tokenExtracted(access_token));
     yield put(actions.loginSuccess(access_token));
     yield put(reset('login'));
@@ -25,16 +25,18 @@ function* loginFlow() {
   let task;
   while (true) {
     const requestToken = localStorage.getItem('requestToken');
-    if (requestToken) { yield put(actions.tokenExtracted(requestToken)); } else {
+    if (requestToken) {
+      yield put(actions.tokenExtracted(requestToken));
+      yield fork(tokenReady, { token: requestToken });
+    } else {
       yield put(actions.tokenNotAvailalbe());
       yield take(actions.LOGIN_REQUEST);
       const { username, password } = yield select(s => s.auth.loginForm);
       task = yield fork(authorize, username, password);
     }
-    const action = yield take([LOGOUT_REQUEST, LOGIN_FAILURE]);
-    if (action.type === LOGOUT_REQUEST && task) { yield cancel(task); }
+    const action = yield take([actions.LOGOUT_REQUEST, actions.LOGIN_FAILURE]);
+    if (action.type === actions.LOGOUT_REQUEST && task) { yield cancel(task); }
     yield call(Api.logout);
-    yield put(push('/personal'));
   }
 }
 
@@ -43,13 +45,20 @@ function* tokenReady({ token }) {
   yield put(actions.profileReady(profile));
 }
 
-function* waitForToken() {
-  yield takeLatest(actions.TOKEN_EXTRACTED, tokenReady);
+function* profileUpdate() {
+  while (true) {
+    const token = yield select(s => s.auth.token);
+    if (token) {
+      const profile = yield call(Api.profile, token);
+      yield put(actions.profileReady(profile));
+    }
+    yield call(delay, 30000);
+  }
 }
 
 export default function* root() {
   yield all([
     fork(loginFlow),
-    fork(waitForToken),
+    fork(profileUpdate),
   ]);
 }
