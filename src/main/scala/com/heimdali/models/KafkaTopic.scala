fe28b0b69062c58f18bc015d2ec10724d0c9cba4
Benny Thompson <benny@jotunn.io>
2018-07-09 17:18:10 -0500

    [WIP] Finished impl for tasks

--- src/main/scala/com/heimdali/models/KafkaTopic.scala
+++ src/main/scala/com/heimdali/models/KafkaTopic.scala
@@ -2,13 +2,17 @@ package com.heimdali.models
 
 import cats.effect.IO
 import com.heimdali.clients._
+import com.heimdali.repositories.ProvisionAttemptRepository
+import doobie.util.transactor.Transactor
 import org.apache.sentry.provider.db.service.thrift.SentryPolicyServiceClient
 
 case class KafkaTopic(name: String, partitions: Int, replicationFactor: Int)
 
 case class AppConfig(hiveClient: HiveClient[IO],
-                     sentryService: SentryPolicyServiceClient,
+                     sentryClient: SentryPolicyServiceClient,
                      ldapClient: LDAPClient[IO],
                      hdfsClient: HDFSClient[IO],
                      yarnClient: YarnClient[IO],
-                     kafkaClient: KafkaClient[IO])
+                     kafkaClient: KafkaClient[IO],
+                     transactor: Transactor[IO],
+                     provisionAttemptRepository: ProvisionAttemptRepository)
