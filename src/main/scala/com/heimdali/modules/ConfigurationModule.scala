e2aa76bec39e13b6c07372f02ad823e61c3f08af
Benny Thompson <benny@jotunn.io>
2018-08-13 16:30:35 -0500

    Added full path to configuration resources

--- src/main/scala/com/heimdali/modules/ConfigurationModule.scala
+++ src/main/scala/com/heimdali/modules/ConfigurationModule.scala
@@ -1,12 +1,14 @@
 package com.heimdali.modules
 
+import java.io.File
+
 import com.heimdali.config.AppConfig
 import com.typesafe.config.{Config, ConfigFactory}
 import kafka.utils.ZkUtils
 import kafka.utils.ZKStringSerializer$
-
 import org.I0Itec.zkclient.{ZkClient, ZkConnection}
 import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
 import pureconfig.{CamelCase, ConfigFieldMapping, ProductHint}
 
 trait ConfigurationModule {
@@ -15,10 +17,10 @@ trait ConfigurationModule {
 
   val hadoopConfiguration: Configuration = {
     val config = new Configuration()
-    config.addResource("core-site.xml")
-    config.addResource("hdfs-site.xml")
-    config.addResource("hive-site.xml")
-    config.addResource("sentry-site.xml")
+    config.addResource(new File("./hive-conf/core-site.xml").toURI.toURL)
+    config.addResource(new File("./hive-conf/hdfs-site.xml").toURI.toURL)
+    config.addResource(new File("./hive-conf/hive-site.xml").toURI.toURL)
+    config.addResource(new File("./sentry-conf/sentry-site.xml").toURI.toURL)
     config
   }
 
