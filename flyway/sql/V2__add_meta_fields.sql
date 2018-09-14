0eab65b9e6fe9d63379c36e3a4c5fc35ac39b093
Benny Thompson <benny@jotunn.io>
2018-09-14 11:11:54 -0500

    Fixed update sql

--- flyway/sql/V2__add_meta_fields.sql
+++ flyway/sql/V2__add_meta_fields.sql
@@ -4,12 +4,15 @@ alter table workspace_request
   add column behavior varchar(50);
 
 update workspace_request
-set    behavior = new_behavior
+set    behavior = new_behavior,
+       summary = b.name,
+       description = b.name
 from   (select case
                  when single_user = true then 'user'
                  when db_count = 3 then 'structured'
                  else 'simple'
                end as new_behavior,
+               wr.name,
                wr.id
         from   workspace_request wr
                inner join (select Count(*) as db_count,
