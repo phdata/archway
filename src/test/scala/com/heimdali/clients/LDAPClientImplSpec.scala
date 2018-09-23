ebac0061980774751eedb1854801595765279c74
Benny Thompson <benny@jotunn.io>
2018-09-23 00:41:21 -0500

    Fixed LDAP tests

--- src/test/scala/com/heimdali/clients/LDAPClientImplSpec.scala
+++ src/test/scala/com/heimdali/clients/LDAPClientImplSpec.scala
@@ -1,67 +1,49 @@
 package com.heimdali.clients
 
-import cats._
-import cats.implicits._
 import cats.effect._
-import com.heimdali.test.fixtures.LDAPTest
-import com.heimdali.config._
-import com.heimdali.test.fixtures._
-import com.unboundid.ldap.sdk.{Modification, ModificationType, SearchResultEntry}
+import com.heimdali.test.fixtures.{LDAPTest, _}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FlatSpec, Matchers}
 
 class LDAPClientImplSpec
-    extends FlatSpec
+  extends FlatSpec
     with Matchers
     with MockitoSugar
     with LDAPTest {
 
   behavior of "LDAPClientImpl"
 
-  it should "validate a user" in new Context {
-    val client = new LDAPClientImpl[IO](config, () => connection) with ActiveDirectoryClient[IO]
+  it should "validate a user" in {
+    val client = new LDAPClientImpl[IO](appConfig.ldap, () => ldapConnectionPool.getConnection) with ActiveDirectoryClient[IO]
     val maybeUser = client.validateUser(existingUser, existingPassword).value.unsafeRunSync()
     maybeUser shouldBe defined
   }
 
-  it should "create a group" in new Context {
-    val client = new LDAPClientImpl[IO](config, () => connection) with ActiveDirectoryClient[IO]
+  it should "create a group" in {
+    val client = new LDAPClientImpl[IO](appConfig.ldap, () => ldapConnectionPool.getConnection) with ActiveDirectoryClient[IO]
 
-    client.createGroup(id, "edh_sw_sesame", s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}").value.unsafeRunSync()
+    client.createGroup(10000, "edh_sw_sesame", s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}").value.unsafeRunSync()
 
-    Option(connection.getEntry(s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}")) shouldBe defined
-    connection.delete(s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}")
+    Option(ldapConnectionPool.getConnection.getEntry(s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}")) shouldBe defined
+    ldapConnectionPool.getConnection.delete(s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}")
   }
 
-  it should "add a user" in new Context {
+  it should "add a user" in {
     val groupDN = s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}"
-    val userDN = s"cn=${existingUser},${appConfig.ldap.userPath}"
+    val userDN = s"cn=$existingUser,${appConfig.ldap.userPath.get}"
 
-    val client = new LDAPClientImpl[IO](config, () => connection) with ActiveDirectoryClient[IO]
+    val client = new LDAPClientImpl[IO](appConfig.ldap, () => ldapConnectionPool.getConnection) with ActiveDirectoryClient[IO]
 
-    client.createGroup(id, "edh_sw_sesame", groupDN).value.unsafeRunSync()
+    client.createGroup(10000, "edh_sw_sesame", groupDN).value.unsafeRunSync()
     client.addUser(groupDN, existingUser).value.unsafeRunSync()
 
-    connection.modify(ldapDn, new Modification(ModificationType.DELETE, "member", userDN))
+    ldapConnectionPool.getConnection.delete(s"cn=edh_sw_sesame,${appConfig.ldap.groupPath}")
   }
 
-  it should "find a user" in new Context {
-    val client = new LDAPClientImpl[IO](config, () => connection) with ActiveDirectoryClient[IO]
+  it should "find a user" in {
+    val client = new LDAPClientImpl[IO](appConfig.ldap, () => ldapConnectionPool.getConnection) with ActiveDirectoryClient[IO]
     val maybeUser = client.findUser(existingUser).value.unsafeRunSync()
     maybeUser shouldBe defined
-  }
-
-  trait Context {
-    val config = LDAPConfig(
-      "localhost",
-      389,
-      "dc=jotunn,dc=io",
-      "ou=groups,ou=hadoop,dc=jotunn,dc=io",
-      Some("ou=users,ou=hadoop,dc=jotunn,dc=io"),
-      "cn=admin,dc=jotunn,dc=io",
-      "admin",
-      "jotunn"
-    )
   }
 
 }
--- src/test/scala/com/heimdali/test/fixtures/LDAPTest.scala
+++ src/test/scala/com/heimdali/test/fixtures/LDAPTest.scala
@@ -1,20 +1,23 @@
 package com.heimdali.test.fixtures
 
 import com.typesafe.config.ConfigFactory
-import com.unboundid.ldap.sdk.LDAPConnection
+import com.unboundid.ldap.sdk.{LDAPConnection, LDAPConnectionPool}
 import com.unboundid.util.ssl.{SSLUtil, TrustAllTrustManager}
 
 trait LDAPTest {
-  val config = ConfigFactory.load()
-  val sslUtil = new SSLUtil(new TrustAllTrustManager)
-  val sslSocketFactory = sslUtil.createSSLSocketFactory
-  val connection = new LDAPConnection(
-    sslSocketFactory,
-    config.getString("ldap.server"),
-    config.getInt("ldap.port"),
-    config.getString("ldap.bindDN"),
-    config.getString("ldap.bindPassword")
-  )
+
+  val ldapConnectionPool: LDAPConnectionPool = {
+    val sslUtil = new SSLUtil(new TrustAllTrustManager)
+    val sslSocketFactory = sslUtil.createSSLSocketFactory
+    val connection = new LDAPConnection(
+      sslSocketFactory,
+      appConfig.ldap.server,
+      appConfig.ldap.port,
+      appConfig.ldap.bindDN,
+      appConfig.ldap.bindPassword
+    )
+    new LDAPConnectionPool(connection, 10)
+  }
 
   val existingUser = "benny"
   val existingPassword = "Jotunn321!"
