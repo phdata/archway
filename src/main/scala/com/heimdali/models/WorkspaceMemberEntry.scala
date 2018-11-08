8b455808ee0d281355ac6de661177a168e73992b
Benny Thompson <benny@jotunn.io>
2018-11-08 09:21:49 -0600

    Fixed member listing to include DN instead of username

--- src/main/scala/com/heimdali/models/WorkspaceMemberEntry.scala
+++ src/main/scala/com/heimdali/models/WorkspaceMemberEntry.scala
@@ -3,14 +3,14 @@ package com.heimdali.models
 import io.circe._
 import io.circe.syntax._
 
-case class WorkspaceMemberEntry(username: String, name: String, email: Option[String], data: List[MemberRights], processing: List[MemberRights], topics: List[MemberRights], applications: List[MemberRights])
+case class WorkspaceMemberEntry(distinguishedName: String, name: String, email: Option[String], data: List[MemberRights], processing: List[MemberRights], topics: List[MemberRights], applications: List[MemberRights])
 
 object WorkspaceMemberEntry {
 
   implicit val encoder: Encoder[WorkspaceMemberEntry] =
     Encoder.instance { m =>
       Json.obj(
-        "username" -> m.username.asJson,
+        "distinguished_name" -> m.distinguishedName.asJson,
         "name" -> m.name.asJson,
         "email" -> m.email.asJson,
         "data" -> Json.obj(m.data.map(d => d.name -> d.asJson):_*),
