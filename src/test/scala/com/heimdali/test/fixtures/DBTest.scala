214d0e87cb488cad562c349ffd731f8ca7d1d1b2
Benny Thompson <benny@jotunn.io>
2018-12-14 22:36:53 -0600

    Switched to localhost reference for db

--- src/test/scala/com/heimdali/test/fixtures/DBTest.scala
+++ src/test/scala/com/heimdali/test/fixtures/DBTest.scala
@@ -12,7 +12,7 @@ trait DBTest extends BeforeAndAfterEach { this: Suite =>
 
   val transactor = Transactor.fromDriverManager[IO](
     "com.mysql.jdbc.Driver",
-    "jdbc:postgresql://postgres:5432/heimdali",
+    "jdbc:postgresql://localhost:5432/heimdali",
     "postgres",
     "postgres"
   )
