471ac81a3fa2c7257e4524e94f4fc7464db152f2
Benny Thompson <benny@phdata.io>
2019-03-08 17:33:34 -0600

    Fixed missing generator config in tests

--- provisioning/src/test/scala/com/heimdali/services/DefaultProvisioningServiceSpec.scala
+++ provisioning/src/test/scala/com/heimdali/services/DefaultProvisioningServiceSpec.scala
@@ -34,7 +34,7 @@ class DefaultProvisioningServiceSpec extends FlatSpec with MockFactory with Matc
       hiveClient.createDatabase _ expects(savedHive.name, savedHive.location) returning IO.unit
       hiveDatabaseRepository.databaseCreated _ expects id returning 0.pure[ConnectionIO]
 
-      ldapClient.createGroup _ expects(savedLDAP.id.get, savedLDAP.commonName, savedLDAP.distinguishedName) returning EitherT
+      ldapClient.createGroup _ expects(savedLDAP.commonName, *) returning EitherT
         .right(IO.unit)
       ldapRepository.groupCreated _ expects id returning 0.pure[ConnectionIO]
       sentryClient.createRole _ expects savedLDAP.sentryRole returning IO.unit
@@ -46,8 +46,7 @@ class DefaultProvisioningServiceSpec extends FlatSpec with MockFactory with Matc
       sentryClient.enableAccessToLocation _ expects(savedHive.location, savedLDAP.sentryRole) returning IO.unit
       grantRepository.locationGranted _ expects id returning 0.pure[ConnectionIO]
 
-      ldapClient.createGroup _ expects(savedLDAP.id.get, savedLDAP.commonName, savedLDAP.distinguishedName) returning EitherT
-        .right(IO.unit)
+      ldapClient.createGroup _ expects(savedLDAP.commonName, *) returning EitherT.right(IO.unit)
       ldapRepository.groupCreated _ expects id returning 0.pure[ConnectionIO]
       sentryClient.createRole _ expects savedLDAP.sentryRole returning IO.unit
       ldapRepository.roleCreated _ expects id returning 0.pure[ConnectionIO]
@@ -70,7 +69,7 @@ class DefaultProvisioningServiceSpec extends FlatSpec with MockFactory with Matc
     }
 
     inSequence {
-      ldapClient.createGroup _ expects(savedLDAP.id.get, savedLDAP.commonName, savedLDAP.distinguishedName) returning EitherT
+      ldapClient.createGroup _ expects(savedLDAP.commonName, *) returning EitherT
         .right(IO.unit)
       ldapRepository.groupCreated _ expects id returning 0.pure[ConnectionIO]
       sentryClient.createRole _ expects savedLDAP.sentryRole returning IO.unit
