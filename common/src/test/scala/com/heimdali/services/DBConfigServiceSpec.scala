84abfe645cb34b795c66850e690cbe2f10990d39
Benny Thompson <benny@phdata.io>
2019-03-20 18:12:10 -0500

    Added empty transactor for db config test

--- common/src/test/scala/com/heimdali/services/DBConfigServiceSpec.scala
+++ common/src/test/scala/com/heimdali/services/DBConfigServiceSpec.scala
@@ -6,10 +6,13 @@ import com.heimdali.repositories.ConfigRepository
 import com.heimdali.test.fixtures._
 import doobie._
 import doobie.implicits._
+import doobie.util.transactor.Strategy
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.FlatSpec
 
-class DBConfigServiceSpec extends FlatSpec with MockFactory with DBTest {
+import scala.concurrent.ExecutionContext
+
+class DBConfigServiceSpec extends FlatSpec with MockFactory {
 
   behavior of "DBConfigService"
 
@@ -25,6 +28,8 @@ class DBConfigServiceSpec extends FlatSpec with MockFactory with DBTest {
   trait Context {
 
     val configRepo = mock[ConfigRepository]
+    implicit val contextShift = IO.contextShift(ExecutionContext.global)
+    val transactor = Transactor.fromConnection[IO](null, ExecutionContext.global).copy(strategy0 = Strategy(FC.unit, FC.unit, FC.unit, FC.unit))
     val configService = new DBConfigService[IO](appConfig, configRepo, transactor)
 
   }
