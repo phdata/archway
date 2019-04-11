bd6896004e6140670da31c4bb303a057b8d2aae7
Chethan YM <chethanym@phdata.io>
2019-04-11 15:58:14 +0000

    [HEIM-83] Quote reserved keyword "key" in sql (pull request #22)

--- flyway/mysql/V3__ldap_attributes.sql
+++ flyway/mysql/V3__ldap_attributes.sql
@@ -13,30 +13,30 @@ CREATE TABLE IF NOT EXISTS `ldap_attribute` (
 
 BEGIN;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'dn', distinguished_name FROM ldap_registration;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'objectClass', 'group' FROM ldap_registration;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'objectClass', 'top' FROM ldap_registration;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'sAMAccountName', common_name FROM ldap_registration;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'cn', common_name FROM ldap_registration;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'msSFU30Name', common_name FROM ldap_registration;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'msSFU30NisDomain', 'jotunn' FROM ldap_registration;
 
 SET @gid_number = 1039493;
 
-INSERT INTO ldap_attribute (ldap_registration_id, key, value)
+INSERT INTO ldap_attribute (ldap_registration_id, `key`, value)
 SELECT id, 'gidNumber', (@gid_number:=@gid_number + 1) FROM ldap_registration;
 
 COMMIT;
