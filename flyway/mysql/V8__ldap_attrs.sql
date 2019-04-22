a39bef413602d398f247a48d4dafcd1309708422
Tony Foerster <tony@phdata.io>
2019-04-22 21:59:17 +0000

    [HEIM-106] Switched mysql column names to non-reserved ones (pull request #34)

--- flyway/mysql/V8__ldap_attrs.sql
+++ flyway/mysql/V8__ldap_attrs.sql
@@ -1,2 +1,2 @@
-ALTER TABLE ldap_attribute CHANGE `key` TO `attr_key`;
-ALTER TABLE ldap_attribute CHANGE `value` TO `attr_value`;
+ALTER TABLE ldap_attribute CHANGE `key` `attr_key` VARCHAR(255);
+ALTER TABLE ldap_attribute CHANGE `value` `attr_value` VARCHAR(255);
