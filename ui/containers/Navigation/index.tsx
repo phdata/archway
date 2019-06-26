831cdc566f582b69e3bd71b2988a92a538914a5d
Ruud Kaland <rkaland@phdata.io>
2019-06-26 16:48:55 +0000

    Click the main logo, then redirect to home screen
    HEIM-282 (pull request #74)
    
    Approved-by: Tony Foerster <tony@phdata.io>
    
    Former-commit-id: efac5df752a2382a755297cc444a03959bf64505

--- ui/containers/Navigation/index.tsx
+++ ui/containers/Navigation/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Layout, Menu, Icon } from 'antd';
-import { NavLink, withRouter, RouteComponentProps } from 'react-router-dom';
+import { NavLink, withRouter, RouteComponentProps, Link } from 'react-router-dom';
 import { ClusterInfo, Profile } from './components';
 import * as selectors from '../../redux/selectors';
 import { Profile as UserProfile } from '../../models/Profile';
@@ -33,7 +33,9 @@ interface Props extends RouteComponentProps<any> {
 
 const Navigation = ({ location, profile }: Props) => (
   <Layout.Sider width={250} style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
-    <img src="images/white_logo_transparent.png" style={{ padding: 25, width: '100%' }} alt="Heimdali Logo" />
+    <Link to="/home">
+      <img src="images/white_logo_transparent.png" style={{ padding: 25, width: '100%' }} alt="Heimdali Logo" />
+    </Link>
     <ClusterInfo />
     <Menu
       style={{
