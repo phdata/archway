4eeadc66d468b84ce06eb916de7f8d3a98baabea
Ruud Kaland <rkaland@phdata.io>
2019-07-08 21:55:28 +0000

    Refactor WorkspaceListing Component
    HEIM-321 (pull request #114)
    
    Approved-by: Tony Foerster <tony@phdata.io>
    
    Former-commit-id: 06f58e6f1b1e7733737ae4ca9eb54289c8a7799e

--- ui/containers/WorkspaceListing/index.tsx
+++ ui/containers/WorkspaceListing/index.tsx
@@ -1,15 +1,17 @@
-import { Card, Col, Input, List, Row, Checkbox, Icon, Table } from 'antd';
+import { Col, Row, Icon } from 'antd';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { createStructuredSelector } from 'reselect';
 import moment from 'moment';
-import { Behavior, ListCardToggle, WorkspaceListItem, Colors, FieldLabel, workspaceColumns } from '../../components';
+import { Colors, ListingSearchBar, WorkspaceList } from '../../components';
 import { WorkspaceSearchResult } from '../../models/Workspace';
 import { Profile } from '../../models/Profile';
 import { Cluster } from '../../models/Cluster';
 import * as actions from './actions';
 import * as selectors from './selectors';
+import { workspaceBehaviors, FeatureFlagType } from '../../constants';
+import { FeatureService } from '../../service/FeatureService';
 
 /* tslint:disable:no-var-requires */
 const { CSVLink } = require('react-csv');
@@ -29,42 +31,20 @@ interface Props {
   setListingMode: (mode: string) => void;
 }
 
-class WorkspaceList extends React.PureComponent<Props> {
-  constructor(props: Props) {
-    super(props);
-
-    this.filterUpdated = this.filterUpdated.bind(this);
-    this.behaviorChanged = this.behaviorChanged.bind(this);
-  }
-
-  public filterUpdated(event: React.ChangeEvent<HTMLInputElement>) {
-    this.props.updateFilter(event.target.value, this.props.filters.behaviors, this.props.filters.statuses);
-  }
-
-  public behaviorChanged(behavior: string, checked: boolean) {
-    const behaviors = this.props.filters.behaviors;
-    if (checked) {
-      behaviors.push(behavior);
-    } else {
-      behaviors.splice(behaviors.indexOf(behavior), 1);
-    }
-
-    this.props.updateFilter(this.props.filters.filter, this.props.filters.behaviors, this.props.filters.statuses);
-  }
-
-  public statusChanged(status: string, checked: boolean) {
-    const statuses = this.props.filters.statuses;
-    if (checked) {
-      statuses.push(status);
-    } else {
-      statuses.splice(statuses.indexOf(status), 1);
-    }
-
-    this.props.updateFilter(this.props.filters.filter, this.props.filters.behaviors, this.props.filters.statuses);
-  }
-
+class WorkspaceListing extends React.PureComponent<Props> {
   public componentDidMount() {
-    this.props.listWorkspaces();
+    const {
+      listWorkspaces,
+      updateFilter,
+      filters: { filter, statuses },
+    } = this.props;
+    const featureService = new FeatureService();
+
+    if (!featureService.isEnabled(FeatureFlagType.CustomTemplates) && workspaceBehaviors.includes('custom')) {
+      workspaceBehaviors.splice(workspaceBehaviors.indexOf('custom'), 1);
+      updateFilter(filter, workspaceBehaviors, statuses);
+    }
+    listWorkspaces();
   }
 
   public generateCSV(workspaceList: WorkspaceSearchResult[]) {
@@ -84,85 +64,25 @@ class WorkspaceList extends React.PureComponent<Props> {
       fetching,
       listingMode,
       workspaceList,
-      filters: { filter, behaviors, statuses },
+      filters,
       openWorkspace,
       setListingMode,
       profile,
       cluster,
+      updateFilter,
     } = this.props;
-    const allStatuses: string[] = ['approved', 'pending', 'rejected'];
+    const { filter, behaviors, statuses } = filters;
     const emptyText =
       !filter && behaviors.length === 2 && statuses.length === 3
         ? 'No workspaces yet. Create one from the link on the left.'
         : 'No workspaces found';
-    const renderItem = (workspace: WorkspaceSearchResult) => (
-      <WorkspaceListItem workspace={workspace} onSelected={openWorkspace} />
-    );
 
     return (
       <div>
         <h1 style={{ textAlign: 'center', margin: 0, padding: 24 }}>Workspaces</h1>
         <Row type="flex">
           <Col span={24} xxl={{ span: 12, offset: 6 }}>
-            <Card>
-              <Row gutter={12} type="flex">
-                <Col
-                  span={24}
-                  lg={12}
-                  style={{
-                    display: 'flex',
-                    flex: 1,
-                    flexDirection: 'column',
-                    justifyContent: 'space-around',
-                  }}
-                >
-                  <div>
-                    <FieldLabel>FILTER</FieldLabel>
-                    <Input.Search value={filter} onChange={this.filterUpdated} placeholder="find a workspace..." />
-                  </div>
-                  <div>
-                    <FieldLabel>STATUS</FieldLabel>
-                    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                      {allStatuses.map((status: string) => (
-                        <Checkbox
-                          key={status}
-                          style={{ fontSize: 12, textTransform: 'uppercase' }}
-                          defaultChecked
-                          name={status}
-                          checked={statuses.indexOf(status) >= 0}
-                          onChange={(e: any) => this.statusChanged(status, e.target.checked)}
-                        >
-                          {status}
-                        </Checkbox>
-                      ))}
-                    </div>
-                  </div>
-                </Col>
-                <Col span={24} lg={12}>
-                  <FieldLabel>BEHAVIOR</FieldLabel>
-                  <Row type="flex" style={{ flexDirection: 'row' }} gutter={12}>
-                    <Col span={12}>
-                      <Behavior
-                        behaviorKey="structured"
-                        selected={behaviors.indexOf('structured') >= 0}
-                        onChange={this.behaviorChanged}
-                        icon="deployment-unit"
-                        title="Structured"
-                      />
-                    </Col>
-                    <Col span={12}>
-                      <Behavior
-                        behaviorKey="simple"
-                        selected={behaviors.indexOf('simple') >= 0}
-                        onChange={this.behaviorChanged}
-                        icon="team"
-                        title="Simple"
-                      />
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Card>
+            <ListingSearchBar filters={filters} updateFilter={updateFilter} />
           </Col>
         </Row>
         {workspaceList && workspaceList.length > 0 && profile.permissions && profile.permissions.platform_operations && (
@@ -175,33 +95,14 @@ class WorkspaceList extends React.PureComponent<Props> {
             </Col>
           </Row>
         )}
-        <ListCardToggle
-          style={{ margin: '12px 0' }}
-          selectedMode={listingMode}
-          onSelect={mode => setListingMode(mode)}
+        <WorkspaceList
+          workspaceList={workspaceList}
+          listingMode={listingMode}
+          emptyText={emptyText}
+          fetching={fetching}
+          setListingMode={setListingMode}
+          openWorkspace={openWorkspace}
         />
-        {listingMode === 'cards' && (
-          <Row>
-            <Col span={24} style={{ padding: '0 12px' }}>
-              <List
-                grid={{ gutter: 12, column: 2 }}
-                locale={{ emptyText }}
-                loading={fetching}
-                dataSource={workspaceList}
-                renderItem={renderItem}
-              />
-            </Col>
-          </Row>
-        )}
-        {listingMode === 'list' && (
-          <Table
-            columns={workspaceColumns}
-            dataSource={workspaceList}
-            onRow={record => ({
-              onClick: () => openWorkspace(record.id),
-            })}
-          />
-        )}
       </div>
     );
   }
@@ -228,4 +129,4 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(WorkspaceList);
+)(WorkspaceListing);
