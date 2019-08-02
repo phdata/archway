8c5350237313feb8d4c9f0b11e0dc7215383a52a
Tony Foerster <tony@phdata.io>
2019-08-02 19:04:16 +0000

    Add full request logging when trace is enabled
    Merged in task/HEIM-450 (pull request #208)
    
    Approved-by: Tony Foerster <tony@phdata.io>
    
    Former-commit-id: 1fcaa5dec8c047961c8ff2862d7ce97320028a92

--- common/src/main/scala/com/heimdali/clients/HttpClient.scala
+++ common/src/main/scala/com/heimdali/clients/HttpClient.scala
@@ -1,6 +1,6 @@
 package com.heimdali.clients
 
-import cats.effect.{Async, Resource}
+import cats.effect.{Async, ConcurrentEffect, Resource}
 import cats.implicits._
 import com.heimdali.config.ClusterConfig
 import com.typesafe.scalalogging.LazyLogging
@@ -9,16 +9,24 @@ import org.http4s._
 import org.http4s.circe._
 import org.http4s.client.Client
 import org.http4s.headers.Authorization
+import org.http4s.client.middleware._
 
 trait HttpClient[F[_]] {
   def request[A](request: Request[F])(implicit decoder: Decoder[A]): F[A]
 }
 
-class CMClient[F[_]: Async](client: Resource[F, Client[F]], clusterConfig: ClusterConfig)
+class CMClient[F[_]: Async: ConcurrentEffect](client: Resource[F, Client[F]], clusterConfig: ClusterConfig)
     extends HttpClient[F] with LazyLogging {
   override def request[A](request: Request[F])(implicit decoder: Decoder[A]): F[A] = {
     implicit val entityDecoder: EntityDecoder[F, A] = jsonOf[F, A]
-    client.use { ready =>
+    client.use { client =>
+      val ready = if (logger.underlying.isTraceEnabled()) {
+        val debugLog = Some((message: String) => logger.trace(message).pure[F])
+        Logger(true, true, logAction = debugLog)(client)
+      } else {
+        client
+      }
+
       for {
         raw <- Async[F].pure(
           request.withHeaders(
@@ -26,13 +34,8 @@ class CMClient[F[_]: Async](client: Resource[F, Client[F]], clusterConfig: Clust
               Headers.of(Authorization(BasicCredentials(clusterConfig.admin.username, clusterConfig.admin.password)))
           )
         )
-        _ <- Async[F].pure(logger.debug("raw request to CM API: {}", raw))
-        response <- ready.fetch[A](raw) { resp =>
-          if (!resp.status.isSuccess) {
-            logger.error("Can't connect to CM {}", resp.body)
-          }
-          resp.as[A]
-        }
+        _ <- Async[F].pure(logger.debug("raw request to CM API. Enable trace logging for further information: {}", raw))
+        response <- ready.fetch[A](raw)(resp => resp.as[A])
       } yield response
     }
   }
