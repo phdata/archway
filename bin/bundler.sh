445a2a0c57fe39141d7f0093a6732cca1677bc5a
Tony Foerster <tony@phdata.io>
2019-09-12 13:51:01 +0000

    Fix bundle variable name and classpath
    Merged in task/bundle-fix (pull request #58)
    
    Approved-by: Ruud Kaland <rkaland@phdata.io>
    
    Former-commit-id: 90e721395e1a878776bef6f743d2a62ae5640509

--- bin/bundler.sh
+++ bin/bundler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xeu
-ARCHWAY_CLASSPATH="${ARCHWAY_SERVER_HOME}"
+ARCHWAY_CLASSPATH="${ARCHWAY_SERVER_HOME}/*"
 exec $JAVA_HOME/bin/java -cp $ARCHWAY_CLASSPATH io.phdata.services.BundleService "$ARTIFACTORY_TOKEN"
