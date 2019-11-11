775a2db47b3de856a2c4ec025ba3473ec704b853
rkaland <rkaland@phdata.io>
2019-11-11 10:42:32 -0600

    Remove group_id in compliance_v2 table

--- flyway/oracle/V7__compliance_v2.sql
+++ flyway/oracle/V7__compliance_v2.sql
@@ -6,5 +6,4 @@ CREATE TABLE compliance_v2 (
   id NUMBER GENERATED ALWAYS AS IDENTITY,
   question_id NUMBER NOT NULL,
   workspace_id NUMBER NOT NULL,
-  group_id NUMBER NOT NULL,
   PRIMARY KEY (id));
--- flyway/sql/V15__compliance_v2.sql
+++ flyway/sql/V15__compliance_v2.sql
@@ -8,5 +8,4 @@ CREATE TABLE IF NOT EXISTS compliance_v2 (
   id BIGINT NOT NULL DEFAULT NEXTVAL ('compliance_v2_seq'),
   question_id BIGINT NOT NULL,
   workspace_id BIGINT NOT NULL,
-  group_id BIGINT NOT NULL,
   PRIMARY KEY (id));
